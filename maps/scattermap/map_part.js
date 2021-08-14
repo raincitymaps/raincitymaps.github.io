7ca987248aeb47118de503ae8df14a036f9d8005
raincitymaps <raincitymaps@gmail.com>
2021-08-14 12:39:13 -0700

    Update map_part.js

--- maps/scattermap/map_part.js
+++ maps/scattermap/map_part.js
@@ -21,7 +21,7 @@ scrollWheelZoom: true
 
 L.tileLayer('https://stamen-tiles.a.ssl.fastly.net/toner/{z}/{x}/{y}.png', {
     maxZoom: 19,
-    attribution: 'Map developed by <a href="https://harrymaher.github.io" target="_blank">Harry Maher</a>. Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
+    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
 }).addTo(map);
 
 
